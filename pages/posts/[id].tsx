7d4dc419150ff8279b565ba9ae329ca437d5ed23
Ilian Rusev <ilianrusev@gmail.com>
2023-01-16 20:20:51 +0200

    polishing pages

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,9 @@ import Layout from "../../components/layout";
 import Head from "next/head";
 
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+
+import Date from "../../components/date";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -25,14 +28,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
